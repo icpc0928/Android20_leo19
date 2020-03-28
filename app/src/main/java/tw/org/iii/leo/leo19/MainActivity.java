6961e3ddb28265698cc69f27faebab4b59160df7
Leo <icpc0928@gmail.com>
2020-03-28 10:40:35 +0800

    3

--- app/src/main/java/tw/org/iii/leo/leo19/MainActivity.java
+++ app/src/main/java/tw/org/iii/leo/leo19/MainActivity.java
@@ -1,17 +1,21 @@
 package tw.org.iii.leo.leo19;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager ;
-    private Fragment[] fs = new Fragment[3];
+    private Fragment[] fs = new Fragment[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +23,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewPager = findViewById(R.id.viewPager);
-        fs[0] = new P1();
-        fs[1] = new P2();
-        fs[2] = new P3();
+        fs[0] = new P0();
+        fs[1] = new P1();
+        fs[2] = new P2();
+        fs[3] = new P3();
+        fs[4] = new P4();
+
+        initActionBar();
 
 //        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
         viewPager.setAdapter(new MyPagerAdapter());
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                Log.v("leo","P: "+ position);
+                if(position == 0){
+                    viewPager.setCurrentItem(1);
+                }else if(position == (fs.length-1)){
+                    viewPager.setCurrentItem(fs.length-2);
+                }
+            }
+        });
+        viewPager.setCurrentItem(2,true);
 
+    }
+
+    private ActionBar actionBar;
+    private void initActionBar(){
+        actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        MyTabListener myTabListener = new MyTabListener();
+        actionBar.addTab(actionBar.newTab().setText("Page1").setTabListener(myTabListener));
+        actionBar.addTab(actionBar.newTab().setText("Page2").setTabListener(myTabListener));
+        actionBar.addTab(actionBar.newTab().setText("Page3").setTabListener(myTabListener));
+
+    }
+
+    private class MyTabListener implements ActionBar.TabListener{
+
+        @Override
+        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        }
+
+        @Override
+        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        }
+
+        @Override
+        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        }
+    }
+
+    public void gotoPage1(View view) {
+        viewPager.setCurrentItem(1,true);
+    }
+    public void gotoPage2(View view) {
+        viewPager.setCurrentItem(2,true);
+    }
+    public void gotoPage3(View view) {
+        viewPager.setCurrentItem(3,true);
     }
 
 
@@ -50,4 +110,6 @@ public class MainActivity extends AppCompatActivity {
             return fs.length;
         }
     }
+
+
 }
